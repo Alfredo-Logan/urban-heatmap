782b6a07fca90424d6f5c0c911610102fca35c80
logan <92327020+DevLoggith@users.noreply.github.com>
2026-08-07 16:13:02 -0400

    set configs for eslint, prettier, and ruff

---
+++ frontend/prettier.config.js
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+
+const config = {
+    printWidth: 100,
+    semi: true,
+    singleQuote: false,
+    tabWidth: 4,
+    trailingComma: "es5",
+};
+
+export default config;
