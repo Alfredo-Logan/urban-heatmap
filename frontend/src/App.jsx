40fa07ca4b1d2deb7265e32ae1ae2a8d7057de8c
logan <92327020+DevLoggith@users.noreply.github.com>
2026-09-17 00:40:29 -0400

    create header & mock menu button groups

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,20 @@
 import Map from "./components/Map";
+import { UserKey, Menu } from "lucide-react";
 
 function App() {
     return (
-        <main className="bg-pastel-green min-h-screen flex flex-col items-center">
-            <h1 className="p-8 text-5xl font-bold">Urban HeatMap</h1>
+        <main className="bg-pastel-green h-screen flex flex-col items-center md:px-8">
+            <header className="self-stretch flex justify-between items-center m-3 md:mx-0 md:my-8">
+                <div className="flex gap-1 items-center">
+                    <Menu className="size-4 md:size-8" />
+                    <p className="text-xs md:text-xl">Navigation</p>
+                </div>
+                <h1 className="md:text-5xl font-bold">Urban HeatMap</h1>
+                <div className="flex gap-1 items-center">
+                    <UserKey className="size-4 md:size-8" />
+                    <p className="text-xs md:text-xl">User Login</p>
+                </div>
+            </header>
             <Map />
         </main>
     );
